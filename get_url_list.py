5004ae1fa06da20694dae8556cd52b10c9f78dc0
Ravi <rkk.bme@gmail.com>
2021-08-08 17:46:32 -0400

    cleaning

--- get_url_list.py
+++ get_url_list.py
@@ -2,15 +2,15 @@
 
 # Getting the List of every submission
 
-def get_url_list(ids, reddit):
+def get_url_list(ids,  reddit):
+
     ids2 = [i if i.startswith('t3_') else f't3_{i}' for i in ids]
-
-    submission_url_list = []
+    url_list = []
     for submission in reddit.info(ids2):
-        submission_url_list.append(submission.url)
-        # print(submission.url)
-
-    return submission_url_list
+        url_list.append(submission.url)
+        # print(submission.title, submission.url)
+    
+    return url_list
 
 # totalSubmissions = len(submission_results)
 # for ID in submission_results:
